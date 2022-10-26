51482b59f778149be6107a381aa5e4e16e1a89fc
Gabriel Guimarães <gabrielglfg@outlook.com>
2022-10-26 10:17:24 -0300

    Menu start

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,14 +1,20 @@
 
-
+import { Wrapper } from "./styles"
 
 
 export function Menu(){
     return(
         <>
-        
-        
-        
-        
+        <Wrapper>
+            <div className="nav-btns">
+                ...
+            </div>
+            <div>
+                <p>
+                    canna
+                </p>
+            </div>
+        </Wrapper>
         </>
     )
 }
---
+++ src/components/menu/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    /* width: 100vw; */
+    padding: 1rem;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    justify-content: space-between;
+
+`
